d0fc2a53b4185c6f196e9a97cc205254543c7a3c
Lunatic Zheng <xiaoyaozi106@163.com>
2014-07-24 18:31:27 +0800

    add test files

---
+++ tests/testjson.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+'''
+Created on 2014年7月24日
+
+@author: hzzhenglh
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ tests/testparser.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+'''
+Created on 2014年7月24日
+
+@author: hzzhenglh
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
