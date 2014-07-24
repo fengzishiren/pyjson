d75c63ff5595e9adeb55cf942d4ee00532f2fae2
Lunatic Zheng <lunaticzheng@gmail.com>
2014-07-24 22:26:42 +0800

    test data automatically indent json

--- tests/testjson.py
+++ tests/testjson.py
@@ -5,21 +5,18 @@ Created on 2014年7月24日
 @author: hzzhenglh
 '''
 import unittest
-
+from parser import Parser
+import json
 
 class Test(unittest.TestCase):
 
 
-    def setUp(self):
-        pass
-
-
-    def tearDown(self):
-        pass
-
-
-    def testName(self):
-        pass
+    def testJson(self):
+        with open("testbig.json") as f:
+            text = '\n'.join(f.readlines())
+        ret = Parser(text).program()
+        print ret
+        print json.dumps(ret)
 
 
 if __name__ == "__main__":
