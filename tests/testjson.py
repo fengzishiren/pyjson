0634505a68a5f2994cda0f509235ef40ca11c477
hzzhenglh <hzzhenglh@corp.netease.com>
2014-07-29 11:30:02 +0800

    update

--- tests/testjson.py
+++ tests/testjson.py
@@ -5,8 +5,8 @@ Created on 2014年7月24日
 @author: hzzhenglh
 '''
 import unittest
-from parser import Parser
 import json
+from parser import Parser
 
 class Test(unittest.TestCase):
 
@@ -14,6 +14,8 @@ class Test(unittest.TestCase):
     def testJson(self):
         with open("testbig.json") as f:
             text = '\n'.join(f.readlines())
+        #text = "{\"firstName\":\"Brett\",\"lastName\":\"McLaughlin\",\"email\":\"aaaa\\\"\", \"age\":18, \"sex\":true, \"wife\":null}";
+
         ret = Parser(text).program()
         print ret
         print json.dumps(ret)
